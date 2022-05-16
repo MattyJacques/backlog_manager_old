12bf5c97c63ffd14e7ccf40d677829216b1a5191
Matthew Jacques <iFtw@live.co.uk>
2022-05-16 17:02:38 +0100

    Merge IGDB::Games and IGDB::Importer classes and get rid of games_genres model (#16)
    * Merge IGDB:Games and Importer
    
    * Delete games_genres.rb

--- app/lib/import/igdb/games.rb
+++ app/lib/import/igdb/games.rb
@@ -12,6 +12,55 @@ module Import
         params[:limit] = 20
         Import::IGDB::Client.post(ENDPOINT, params)
       end
+
+      def self.import(id)
+        return if id.blank?
+
+        data = get_igdb_data(id)
+        import_from_data(data)
+      end
+
+      private
+
+      def self.get_igdb_data(id)
+        params = { fields: 'name, genres.name, platforms.platform_family.name,
+                            platforms.name, release_dates.date, release_dates.game,
+                            release_dates.platform, release_dates.region' }
+        params[:where] = "id = #{id}"
+        Import::IGDB::Client.post(ENDPOINT, params).first
+      end
+
+      def self.import_from_data(data)
+        game = import_game(data)
+        game.genres = import_genres(data.genres)
+        game.platforms = import_platforms(data.platforms)
+        game.release_dates = import_release_dates(game, data.release_dates)
+
+        game
+      end
+
+      def self.import_game(data)
+        Game.create!(name: data.name, igdb_id: data.id)
+      end
+
+      def self.import_genres(genres)
+        genres.map { |genre| Genre.find_or_create_by!(igdb_id: genre.id, name: genre.name) }
+      end
+
+      def self.import_platforms(platforms)
+        platforms.map do |platform|
+          family_data = platform.platform_family
+          family = PlatformFamily.find_or_create(family_data&.id, family_data&.name)
+          Platform.find_or_create_by!(name: platform.name, igdb_id: platform.id, platform_family: family)
+        end
+      end
+
+      def self.import_release_dates(game, dates)
+        dates.map do |date|
+          platform = Platform.find_by!(igdb_id: date.platform)
+          ReleaseDate.convert_create(game, platform, date.date)
+        end
+      end
     end
   end
 end
--- app/models/games_genres.rb
+++
@@ -1,7 +0,0 @@
-# Model responsible for handling Game/Genre join records
-# game_id: integer
-# genre_id: integer
-class GamesGenres < ApplicationRecord
-  belongs_to :game
-  belongs_to :genre
-end
