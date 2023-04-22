dafa32ad3b77ed6ed32ee8e6032de3106f532ffe
Matthew Jacques <iFtw@live.co.uk>
2023-04-22 18:07:41 +0100

    Initial commit

---
+++ config/importmap.rb
@@ -0,0 +1,14 @@
+# Pin npm packages by running ./bin/importmap
+
+# Turbo/Stimulus
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+
+# Bootstrap for CSS
+pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.2.3/dist/js/bootstrap.esm.js"
+pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.7/lib/index.js"
+
+# Application JS
+pin "application", preload: true
+pin_all_from "app/javascript/controllers", under: "controllers"
